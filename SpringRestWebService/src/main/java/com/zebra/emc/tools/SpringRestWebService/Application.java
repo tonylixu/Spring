d8369b8cef5ec7105d38724a3cbfbe3098777f75
tonylixu@gmail.com <tonylixu@gmail.com>
2017-03-21 15:49:03 -0400

    Remove unnecessary bean definitions in Application.java

--- SpringRestWebService/src/main/java/com/zebra/emc/tools/SpringRestWebService/Application.java
+++ SpringRestWebService/src/main/java/com/zebra/emc/tools/SpringRestWebService/Application.java
@@ -14,20 +14,4 @@ public class Application {
     public static void main(String args[]) throws Exception {
         SpringApplication.run(Application.class);
     }
-
-    /*
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
-    }
-
-    @Bean
-    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-        return args -> {
-            Quote quote = restTemplate.getForObject(
-                "http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-            log.info(quote.toString());
-        };
-    }
-    */
 }
