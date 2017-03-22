4a37e5326c88b05be4b53f598b1d2a2f69daf0b7
tonylixu@gmail.com <tonylixu@gmail.com>
2017-03-22 08:09:28 -0400

    Added setter and getter methods for Customer class

--- SpringRdbmAccess/src/main/java/com/zebra/emc/tools/SpringRdbmAccess/Customer.java
+++ SpringRdbmAccess/src/main/java/com/zebra/emc/tools/SpringRdbmAccess/Customer.java
@@ -21,4 +21,29 @@ public class Customer {
     }
 
     // getters and setters omitted for brevity
+    public void setId(long id) {
+        this.id=id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName=firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
 }
