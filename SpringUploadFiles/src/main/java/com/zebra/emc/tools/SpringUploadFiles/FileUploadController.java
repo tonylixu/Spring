c901dfcc620e7c87f7af0d642431091323707e0b
tonylixu@gmail.com <tonylixu@gmail.com>
2017-03-22 18:55:02 -0400

    Added responseEntity

--- SpringUploadFiles/src/main/java/com/zebra/emc/tools/SpringUploadFiles/FileUploadController.java
+++ SpringUploadFiles/src/main/java/com/zebra/emc/tools/SpringUploadFiles/FileUploadController.java
@@ -3,10 +3,19 @@ package com.zebra.emc.tools.SpringUploadFiles;
 import java.io.IOException;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Created by Tony Li Xu on 3/22/2017.
@@ -34,5 +43,25 @@ public class FileUploadController {
             .collect(Collectors.toList()));
 
         return "uploadForm";
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename
+                () + "\"").body(file);
+    }
+
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes
+        redirectAttributes) {
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file
+            .getOriginalFilename() + "!");
+
+        return "redirect:/";
     }
 }
