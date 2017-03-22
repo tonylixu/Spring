5d9955db053cf40244d35156be7655f75f1e4449
tonylixu@gmail.com <tonylixu@gmail.com>
2017-03-22 16:44:35 -0400

    Add Application class

---
+++ SpringUploadFiles/src/main/java/com/zebra/emc/tools/SpringUploadFiles/Application.java
@@ -0,0 +1,16 @@
+package com.zebra.emc.tools.SpringUploadFiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Tony Li Xu on 3/22/2017.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
