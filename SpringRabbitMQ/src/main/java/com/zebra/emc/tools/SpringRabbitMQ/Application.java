5d8ad3ce837ae19d212c7ca31175ec31d5d5d061
tonylixu@gmail.com <tonylixu@gmail.com>
2017-04-03 20:57:42 -0400

    Create all Bean injections

--- SpringRabbitMQ/src/main/java/com/zebra/emc/tools/SpringRabbitMQ/Application.java
+++ SpringRabbitMQ/src/main/java/com/zebra/emc/tools/SpringRabbitMQ/Application.java
@@ -1,6 +1,9 @@
 package com.zebra.emc.tools.SpringRabbitMQ;
 
 import java.util.Queue;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
@@ -15,6 +18,31 @@ public class Application {
     @Bean
     Queue quque() {
         return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("spring-boot-exchange");
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(queueName);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+        MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
     }
 
 }
