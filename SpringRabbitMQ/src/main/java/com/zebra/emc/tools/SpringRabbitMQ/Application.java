40af9346aaefb86850de9587875cceafd7aea435
tonylixu@gmail.com <tonylixu@gmail.com>
2017-04-03 20:58:14 -0400

    Add main application method

--- SpringRabbitMQ/src/main/java/com/zebra/emc/tools/SpringRabbitMQ/Application.java
+++ SpringRabbitMQ/src/main/java/com/zebra/emc/tools/SpringRabbitMQ/Application.java
@@ -4,6 +4,7 @@ import java.util.Queue;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
@@ -43,6 +44,10 @@ public class Application {
     @Bean
     MessageListenerAdapter listenerAdapter(Receiver receiver) {
         return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(Application.class, args);
     }
 
 }
